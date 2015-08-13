b9b5a95c5bc34719f342345d1baaa1ecb40559a2
zhangtengteng <zhangtengteng_vip@163.com>
2015-08-13 16:54:52 +0800

    整合代码

--- Mysvn/gen/com/example/webservice/R.java
+++ Mysvn/gen/com/example/webservice/R.java
@@ -15,13 +15,13 @@ public final class R {
 		public static final int ic_launcher = 0x7f020028;
 	}
 	public static final class id {
-		public static final int action_settings = 0x7f0c01d1;
+		public static final int action_settings = 0x7f0c01d8;
 	}
 	public static final class layout {
-		public static final int activity_main = 0x7f03000f;
+		public static final int activity_main = 0x7f030011;
 	}
 	public static final class menu {
-		public static final int main = 0x7f0b0003;
+		public static final int main = 0x7f0b0004;
 	}
 	public static final class string {
 		public static final int action_settings = 0x7f070001;
